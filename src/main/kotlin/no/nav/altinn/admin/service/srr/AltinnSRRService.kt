236fa36e79f7ce224b6cc41508e460bc6bf2b04e
richos7 <35131764+richos7@users.noreply.github.com>
2019-06-06 13:27:06 +0200

    Update AltinnSRRService.kt

--- src/main/kotlin/no/nav/altinn/admin/service/srr/AltinnSRRService.kt
+++ src/main/kotlin/no/nav/altinn/admin/service/srr/AltinnSRRService.kt
@@ -66,6 +66,7 @@ class AltinnSRRService(env: Environment, iRegisterSRRAgencyExternalBasicFactory:
         try {
             val register = env.mock.srrGetResponse ?: iRegisterSRRAgencyExternalBasic.getRightsBasic(altinnUsername, altinnUserPassword,
                 "5252", 1, "")
+            logger.info { register.toString() }
             val result = mutableListOf<RegistryResponse.Register>()
             register.getRightResponse.forEach { it ->
                 logger.info { "reportee: ${it.reportee} og condition: ${it.condition}" }
