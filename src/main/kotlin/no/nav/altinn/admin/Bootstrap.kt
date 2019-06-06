3188bab6878ea5f1efe874fbbeb27f3f09ff07bc
richos7 <35131764+richos7@users.noreply.github.com>
2019-06-06 08:51:21 +0200

    Update Bootstrap.kt

--- src/main/kotlin/no/nav/altinn/admin/Bootstrap.kt
+++ src/main/kotlin/no/nav/altinn/admin/Bootstrap.kt
@@ -5,8 +5,6 @@ import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.auth.Authentication
-import io.ktor.auth.UserIdPrincipal
-import io.ktor.auth.basic
 import io.ktor.auth.jwt.JWTPrincipal
 import io.ktor.auth.jwt.jwt
 import io.ktor.client.utils.CacheControl
