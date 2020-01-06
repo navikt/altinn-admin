5ee05d0cc1662d03e0cfb2d1595ec0c301147223
richos7 <35131764+richos7@users.noreply.github.com>
2020-01-06 07:54:26 +0100

    chore: move deploy to github action

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import no.nils.wsdl2java.Wsdl2JavaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "no.nav"
-version = "1.0.0"
+version = "1.0.1-SNAPSHOT"
 
 val kotlinVersion = "1.3.50"
 val ktorVersion = "1.2.1"
