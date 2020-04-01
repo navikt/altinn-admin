1de7a9e19dfb7e2b01a63bfc88d01fc22d3f335d
richos7 <35131764+richos7@users.noreply.github.com>
2020-04-01 13:11:32 +0200

    change to timer schedule

--- src/main/kotlin/no/nav/altinn/admin/service/quick/payout/QuickPayout.kt
+++ src/main/kotlin/no/nav/altinn/admin/service/quick/payout/QuickPayout.kt
@@ -1,7 +1,6 @@
 package no.nav.altinn.admin.service.quick.payout
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import kotlinx.coroutines.delay
 import mu.KotlinLogging
 import no.nav.altinn.admin.Environment
 import no.nav.altinn.admin.common.ApplicationState
@@ -9,6 +8,7 @@ import no.nav.altinn.admin.common.objectMapper
 import no.nav.altinn.admin.metrics.Metrics
 import no.nav.altinn.admin.service.dq.AltinnDQService
 import java.io.StringReader
+import java.lang.Thread.sleep
 import java.util.*
 import javax.xml.stream.XMLInputFactory
 import javax.xml.stream.events.XMLEvent
@@ -21,63 +21,68 @@ class QuickPayout(
     private val applicationState: ApplicationState,
     private val dqService: AltinnDQService
 ) {
-    suspend fun fetchAndCreateList() {
-        while (applicationState.running) {
-            val today = Calendar.getInstance().time
-            logger.debug { "Running thread fetch quick payout records and create list...$today" }
-            val response = dqService.getDownloadQueueItems("5546", "1")
+    fun fetchAndCreateList() {
+        val today = Calendar.getInstance()
+        today.set(Calendar.HOUR_OF_DAY, 13)
+        today.set(Calendar.MINUTE, 30)
+        today.set(Calendar.SECOND, 0)
+        val timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                logger.debug { "Running thread fetch quick payout records and create list...$today" }
+                val response = dqService.getDownloadQueueItems("5546", "1")
 
-            val quickList = mutableListOf<Quick>()
-            val failedAR = mutableListOf<String>()
-            response.items.forEach item@{ ar ->
-                logger.debug { "Fetching rules for serviceCode $ar" }
-                val melding = dqService.getFormData(ar.archiveReference)
-                if (melding.status == "Ok") {
-                    logger.debug { "Message received from DQ ${ar.archiveReference}." }
-                } else {
-                    logger.error { "Failed to get message ${ar.archiveReference}, from DQ." }
-                    failedAR.add(ar.archiveReference)
-                    return@item
-                }
+                val quickList = mutableListOf<Quick>()
+                val failedAR = mutableListOf<String>()
+                response.items.forEach item@{ ar ->
+                    logger.debug { "Fetching rules for serviceCode $ar" }
+                    val melding = dqService.getFormData(ar.archiveReference)
+                    if (melding.status == "Ok") {
+                        logger.debug { "Message received from DQ ${ar.archiveReference}." }
+                    } else {
+                        logger.error { "Failed to get message ${ar.archiveReference}, from DQ." }
+                        failedAR.add(ar.archiveReference)
+                        return@item
+                    }
 
-                val xml = melding.arData.formData
-                val xmlReader = xmlInputFactory.createXMLStreamReader(StringReader(xml))
-                var organisasjonsnummer = ""
-                var hyppig = false
-                while (xmlReader.hasNext()) {
-                    val eventType = xmlReader.next()
-                    if (eventType == XMLEvent.START_ELEMENT) {
-                        when (xmlReader.localName) {
-                            "organisasjonsnummer" -> organisasjonsnummer = xmlReader.elementText
-                            "onskerHyppigereRefusjon" -> hyppig = xmlReader.elementText == "true"
+                    val xml = melding.arData.formData
+                    val xmlReader = xmlInputFactory.createXMLStreamReader(StringReader(xml))
+                    var organisasjonsnummer = ""
+                    var hyppig = false
+                    while (xmlReader.hasNext()) {
+                        val eventType = xmlReader.next()
+                        if (eventType == XMLEvent.START_ELEMENT) {
+                            when (xmlReader.localName) {
+                                "organisasjonsnummer" -> organisasjonsnummer = xmlReader.elementText
+                                "onskerHyppigereRefusjon" -> hyppig = xmlReader.elementText == "true"
+                            }
                         }
                     }
+                    quickList.add(Quick(organisasjonsnummer, hyppig))
+                    logger.debug { "Got xml data $xml" }
                 }
-                quickList.add(Quick(organisasjonsnummer, hyppig))
-                logger.debug { "Got xml data $xml" }
-            }
-            if (quickList.size > 0) {
-                val result = QuickList(quickList.size, quickList)
-                val po = objectMapper.writeValueAsString(result)
-                Metrics.quickPayoutSuccess.labels("5546", po).inc()
-                logger.info { "Resultat hyppig utbetaling liste: $po" }
-            } else {
-                logger.info { "Ingen nye meldinger funnet, bedre lykke neste gang." }
-            }
-            delay(1000 * 60 * 1) // wait a minute
-            response.items.forEach { ar ->
-                if (failedAR.contains(ar.archiveReference)) {
-                    return@forEach // skip failed message download
-                }
-                val purgeResponse = dqService.purgeItem(ar.archiveReference)
-                if (purgeResponse.status == "Ok") {
-                    logger.debug { "Message ${ar.archiveReference} deleted from DQ." }
+                if (quickList.size > 0) {
+                    val result = QuickList(quickList.size, quickList)
+                    val po = objectMapper.writeValueAsString(result)
+                    Metrics.quickPayoutSuccess.labels("5546", po).inc()
+                    logger.info { "Resultat hyppig utbetaling liste: $po" }
                 } else {
-                    logger.error { "Failed to delete ${ar.archiveReference}, will come again next day." }
+                    logger.info { "Ingen nye meldinger funnet, bedre lykke neste gang." }
+                }
+                sleep(1000 * 60 * 2) // wait a minute
+                response.items.forEach { ar ->
+                    if (failedAR.contains(ar.archiveReference)) {
+                        return@forEach // skip failed message download
+                    }
+                    val purgeResponse = dqService.purgeItem(ar.archiveReference)
+                    if (purgeResponse.status == "Ok") {
+                        logger.debug { "Message ${ar.archiveReference} deleted from DQ." }
+                    } else {
+                        logger.error { "Failed to delete ${ar.archiveReference}, will come again next day." }
+                    }
                 }
             }
-            delay(1000 * 60 * 2)
-        }
+        }, today.time, 1000 * 60)
     }
 }
 
