24da7279bb683b162f2741395ef3023aa9bc90f0
richos7 <35131764+richos7@users.noreply.github.com>
2020-11-27 14:03:31 +0100

    Update SelfTestSpek.kt

--- src/test/kotlin/no/nav/altinn/admin/api/SelfTestSpek.kt
+++ src/test/kotlin/no/nav/altinn/admin/api/SelfTestSpek.kt
@@ -6,7 +6,6 @@ import io.ktor.routing.routing
 import io.ktor.server.testing.TestApplicationEngine
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.on
-import no.nav.altinn.admin.Environment
 import org.amshove.kluent.shouldBeEqualTo
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -19,13 +18,13 @@ object SelfTestSpek : Spek({
     )
 
     val testCases = listOf(
-        TestCase(description = "both selftests OK", environment = Environment(), readiness = true, liveness = true),
-        TestCase(description = "both selftests ERROR", environment = Environment(), readiness = false, liveness = false),
-        TestCase(description = "only readiness OK", environment = Environment(), readiness = true, liveness = false),
-        TestCase(description = "only liveness OK", environment = Environment(), readiness = false, liveness = true)
+        TestCase(description = "both selftests OK", readiness = true, liveness = true),
+        TestCase(description = "both selftests ERROR", readiness = false, liveness = false),
+        TestCase(description = "only readiness OK", readiness = true, liveness = false),
+        TestCase(description = "only liveness OK", readiness = false, liveness = true)
     )
 
-    testCases.forEach { (description, environment, readiness, liveness) ->
+    testCases.forEach { (description, readiness, liveness) ->
         describe("SelfTest routing with $description") {
             with(TestApplicationEngine()) {
                 start()
@@ -57,5 +56,5 @@ object SelfTestSpek : Spek({
     }
 })
 
-private data class TestCase(val description: String, val environment: Environment, val readiness: Boolean, val liveness: Boolean)
+private data class TestCase(val description: String, val readiness: Boolean, val liveness: Boolean)
 private enum class SelfTests { LIVENESS, READINESS }
