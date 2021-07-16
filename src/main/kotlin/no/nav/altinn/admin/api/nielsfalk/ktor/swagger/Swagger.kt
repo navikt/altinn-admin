de57f9fbc73b195c471145c378a989276b4db203
richos7 <35131764+richos7@users.noreply.github.com>
2021-07-16 12:03:16 +0200

    Update Swagger.kt

--- src/main/kotlin/no/nav/altinn/admin/api/nielsfalk/ktor/swagger/Swagger.kt
+++ src/main/kotlin/no/nav/altinn/admin/api/nielsfalk/ktor/swagger/Swagger.kt
@@ -34,7 +34,7 @@ typealias Methods = MutableMap<MethodName, Operation>
 data class SecurityType(val type: String)
 
 data class Swagger(
-    val swagger: String = "2.0",
+    val swagger: String = "3.0",
     val info: Information,
     val paths: Paths = mutableMapOf(),
     val definitions: Definitions = mutableMapOf(),
