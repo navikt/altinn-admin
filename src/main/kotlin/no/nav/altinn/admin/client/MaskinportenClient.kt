c9310f41c51476ba961cb9dc24e1fdc4c4290469
richos7 <35131764+richos7@users.noreply.github.com>
2021-11-05 11:55:18 +0100

    log request

--- src/main/kotlin/no/nav/altinn/admin/client/MaskinportenClient.kt
+++ src/main/kotlin/no/nav/altinn/admin/client/MaskinportenClient.kt
@@ -33,7 +33,7 @@ class MaskinportenClient(val environment: Environment) {
         }
         install(Logging) {
             logger = Logger.DEFAULT
-            level = LogLevel.NONE
+            level = LogLevel.ALL
         }
     }
 
