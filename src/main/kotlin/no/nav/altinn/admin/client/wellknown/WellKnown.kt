92e73ee8c2bf344b18fd8d903220a8aeec281812
richos7 <35131764+richos7@users.noreply.github.com>
2021-12-03 07:21:08 +0100

    Update WellKnown.kt

--- src/main/kotlin/no/nav/altinn/admin/client/wellknown/WellKnown.kt
+++ src/main/kotlin/no/nav/altinn/admin/client/wellknown/WellKnown.kt
@@ -10,6 +10,10 @@ import io.ktor.client.engine.apache.Apache
 import io.ktor.client.engine.apache.ApacheEngineConfig
 import io.ktor.client.features.json.JacksonSerializer
 import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.logging.DEFAULT
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
 import io.ktor.client.request.get
 import java.net.ProxySelector
 import kotlinx.coroutines.runBlocking
@@ -22,6 +26,10 @@ val proxyConfig: HttpClientConfig<ApacheEngineConfig>.() -> Unit = {
             registerModule(JavaTimeModule())
             configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
         }
+    }
+    install(Logging) {
+        logger = Logger.DEFAULT
+        level = LogLevel.ALL
     }
     engine {
         customizeClient {
